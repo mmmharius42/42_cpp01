d8043390455e05156fa7edd23f486e9228495b85
mmmharius <mariuspapin@rcmm.fr>
2025-07-09 04:14:58 +0200

    cpp01, ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpapin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/09 04:08:06 by mpapin            #+#    #+#             */
+/*   Updated: 2025/07/09 04:12:45 by mpapin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str = "HI THIS IS BRAIN";
+
+    std::string* stringPTR = &str;
+    std::string& stringREF = str;
+
+    std::cout << "Adresse de la variable string: " << &str << std::endl;
+    std::cout << "Adresse contenue dans stringPTR: " << stringPTR << std::endl;
+    std::cout << "Adresse de stringREF: " << &stringREF << std::endl;
+
+    std::cout << "Valeur de la variable string: " << str << std::endl;
+    std::cout << "Valeur pointée par stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Valeur de stringREF: " << stringREF << std::endl;
+    
+    return 0;
+}
